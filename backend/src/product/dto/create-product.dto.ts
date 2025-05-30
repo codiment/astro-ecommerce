697fc558125c2cfec35c88d4ce9a357b82cb205c
codiment <uprovisional51@gmail.com>
2025-05-29 23:18:20 -0300

    add apiproperty

--- backend/src/product/dto/create-product.dto.ts
+++ backend/src/product/dto/create-product.dto.ts
@@ -1,3 +1,38 @@
-import { Product } from "generated/prisma";
+import { ApiProperty } from "@nestjs/swagger";
 
-export type CreateProductDto = Omit<Product, "id" | "createdAt" | "updatedAt">;
+export class CreateProductDto {
+    @ApiProperty({
+        description: 'The name of the product',
+        example: 'Amazing T-shirt',
+    })
+    title: string;
+
+    @ApiProperty({
+        description: 'The price of the product',
+        example: 'High quality t-shirt',
+        required: false,
+    })
+    description?: string;
+
+    @ApiProperty({
+        description: 'The price of the product',
+        example: 999.99,
+        type: Number,
+        format: 'float',
+    })
+    price: number;
+
+    @ApiProperty({
+        description: 'The stock of the product',
+        example: 10,
+        type: 'integer',
+    })
+    stock: number;
+
+    @ApiProperty({
+        description: 'The image of the product',
+        example: 'image.jpg',
+        required: false,
+    })
+    image?: string;
+}
