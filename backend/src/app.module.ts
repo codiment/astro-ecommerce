0f1335818a85ea423ee8310fd7c551e47237c46c
codiment <uprovisional51@gmail.com>
2025-05-30 19:02:16 -0300

    add schema user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PrismaModule,
-    ProductModule
+    ProductModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
