37175bdd17929378a50d69b2dafb530bb150d41d
Codiment <uprovisional51@gmail.com>
2025-06-03 20:39:09 -0300

    add user seed (#7)
    * add apiproperty
    
    * add schema user
    
    * change schema and add user
    
    * add user endpoints
    
    * add user seed

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -6,18 +6,20 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+
+  constructor(private prisma: PrismaService) { }
 
   async create(createUserDto: CreateUserDto) {
     const hashedPassword = await hashPassword(createUserDto.password);
 
-    return this.prisma.user.create({
+    this.prisma.user.create({
       data: {
         ...createUserDto,
-        password: hashedPassword,
-      },
-    });
+        password: hashedPassword
+      }
+    })
   }
+
 
   findAll() {
     return this.prisma.user.findMany();
@@ -26,23 +28,23 @@ export class UserService {
   findOne(id: number) {
     return this.prisma.user.findUnique({
       where: {
-        id,
-      },
-    });
+        id
+      }
+    })
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.prisma.user.update({
       where: { id },
-      data: updateUserDto,
-    });
+      data: updateUserDto
+    })
   }
 
   remove(id: number) {
     return this.prisma.user.delete({
       where: {
-        id,
-      },
-    });
+        id
+      }
+    })
   }
 }
