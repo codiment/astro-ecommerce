bc503ace117f6624bc3db6c6324b190566ab06dc
Codiment <uprovisional51@gmail.com>
2025-07-04 20:02:11 -0300

    add order function (#14)

--- backend/src/cart/dto/add-to-cart.dto.ts
+++ backend/src/cart/dto/add-to-cart.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, Min } from 'class-validator';
 
 export class addToCartDto {
-  @ApiProperty({ example: 'productId: 1' })
+  @ApiProperty({ example: '1' })
   @IsInt()
   productId: number;
 
-  @ApiProperty({ example: 'quantity: 2' })
+  @ApiProperty({ example: '2' })
   @IsInt()
   @Min(1)
   quantity: number;
--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,7 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { OrderService } from './order.service';
+import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
+
+// Definir interfaz para que req.user
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: number;
+  };
+}
 
 @Controller('order')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  getUserOrders(@Req() req: AuthenticatedRequest) {
+    return this.orderService.getUserOrders(req.user.id);
+  }
+
+  @Post()
+  createOrder(@Req() req: AuthenticatedRequest) {
+    return this.orderService.createOrder(req.user.id);
+  }
+
+  @Patch('pay/:id')
+  @ApiOperation({ summary: 'Mark an order as paid' })
+  @ApiParam({ name: 'id', description: 'Order ID' })
+  async payOrder(@Param('id') id: string, @Req() req: AuthenticatedRequest) {
+    const orderId = parseInt(id);
+    if (isNaN(orderId)) {
+      throw new NotFoundException('Invalid order ID');
+    }
+    return this.orderService.payOrder(req.user.id, orderId);
+  }
+
+  @Patch(':id/status')
+  @ApiOperation({ summary: 'Update order status manually' })
+  @ApiParam({ name: 'id', description: 'Order ID' })
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateOrderStatusDto,
+    @Req() req: AuthenticatedRequest,
+  ) {
+    return this.orderService.updateOrderStatus(req.user.id, id, dto.status);
+  }
 }
