23c10cf696b78fb3c51f8497b75aee16bc7fdbab
codiment <uprovisional51@gmail.com>
2025-08-13 23:54:17 -0300

    add viewTransitions

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind()],
-    output: 'server',
-    adapter: cloudflare()
+    output: 'static',
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: 'viewport'
+    },
+    experimental: {
+        clientPrerender: true // Mejora ViewTransitions
+    }
 });
